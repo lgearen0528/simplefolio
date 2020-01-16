73f05307a3c96310085946ccad8e784882297c13
Gearen Liem J <GearenLiemJ@JohnDeere.com>
2020-01-16 14:29:26 -0600

    updated some basic info

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import initTilt from './js/tilt';
-import initSr from './js/sr';
-import './style/main.scss';
+import initTilt from "./js/tilt";
+import initSr from "./js/sr";
+import "./style/main.scss";
 
-$('a[href^="#"]').on('click', function(event) {
-  var target = $(this.getAttribute('href'));
+$('a[href^="#"]').on("click", event => {
+  var target = $(this.getAttribute("href"));
   if (target.length) {
     event.preventDefault();
-    $('html, body')
+    $("html, body")
       .stop()
       .animate(
         {
